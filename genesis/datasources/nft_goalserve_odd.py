59bb0741091461ddcd2f6b1c7e400043109826ee
prin-r <q@bandprotocol.com>
2021-02-03 00:52:33 +0700

    -

--- genesis/datasources/nft_goalserve_odd.py
+++ genesis/datasources/nft_goalserve_odd.py
@@ -5,18 +5,17 @@ import urllib.request
 import sys
 from time import time
 
-GOALSERVE_URL = "https://www.goalserve.com/getfeed/30d7e29edaba41579db708d879c8db2e/{}/nfl-shedule?date1={}&date2={}&showodds=1&json=1"
+GOALSERVE_URL = "https://www.goalserve.com/getfeed/30d7e29edaba41579db708d879c8db2e/{}/nfl-shedule?date1={}&date2={}&showodds=1&bm={},&json=1"
+
+MOCK_URL = "http://liquidsports.bet/Football"
 
 
 def make_json_request(url):
     return json.loads(urllib.request.urlopen(url).read())
 
 
-def main(category, date1, date2, contest_id, odds_type, bookmaker_id):
-    x = time()
-    res = make_json_request(GOALSERVE_URL.format(category, date1, date2))
-    print(time() - x)
-    x = time()
+def main(category, date1, date2, tournament_name, contest_id, odds_type, bookmaker_id):
+    res = make_json_request(MOCK_URL)
     tournaments = res["shedules"]["tournament"]
 
     def find_main_1(l):
@@ -27,60 +26,57 @@ def main(category, date1, date2, contest_id, odds_type, bookmaker_id):
                 return x.get("odd", [])
         return []
 
-    def find_odds():
-        for tournament in tournaments:
-            for week in tournament.get("week", []):
-                matches = week.get("matches", [])
-                if type(matches) is not list:
-                    matches = [matches]
-                for match in matches:
-                    if type(match) is dict:
-                        matches_inner = match.get("match")
-                        if type(matches_inner) is not list:
-                            matches_inner = [matches_inner]
-                        for m in matches_inner:
-                            if m.get("contestID") == contest_id:
-                                types = m.get("odds", {}).get("type", [])
-                                if type(types) is not list:
-                                    types = [types]
-                                for t in types:
-                                    if (
-                                        type(t) is dict
-                                        and t.get("value").lower() == odds_type.lower()
-                                    ):
-                                        bookmakers = t.get("bookmaker", [])
-                                        if type(bookmakers) is not list:
-                                            bookmakers = [bookmakers]
-                                        for bookmaker in bookmakers:
-                                            if (
-                                                type(bookmaker) is dict
-                                                and bookmaker.get("id") == bookmaker_id
-                                            ):
-                                                if odds_type.lower() == "over/under":
-                                                    return find_main_1(bookmaker.get("total", {}))
-                                                elif odds_type.lower() == "handicap":
-                                                    return find_main_1(
-                                                        bookmaker.get("handicap", {})
-                                                    )
-        return []
-
-    odds = find_odds()
-    if len(odds) < 2:
-        return None
-
-    result = [
-        odds[0].get("value", None),
-        odds[0].get("dp3", None),
-        odds[1].get("value", None),
-        odds[1].get("dp3", None),
-    ]
-
-    if None in result:
-        return None
-
-    print(time() - x)
-
-    return "{} {} {} {}".format(result[0], result[1], result[2], result[3])
+    for tournament in tournaments:
+        if tournament_name.lower().replace(" ", "") != tournament.get("name", None).lower().replace(
+            " ", ""
+        ):
+            continue
+        for week in tournament.get("week", []):
+            matches = week.get("matches", [])
+            if type(matches) is not list:
+                matches = [matches]
+            for match in matches:
+                if type(match) is dict:
+                    matches_inner = match.get("match")
+                    if type(matches_inner) is not list:
+                        matches_inner = [matches_inner]
+                    for m in matches_inner:
+                        if m.get("contestID") == contest_id:
+                            types = m.get("odds", {}).get("type", [])
+                            if type(types) is not list:
+                                types = [types]
+                            for t in types:
+                                if type(t) is dict and t.get("value").lower() == odds_type.lower():
+                                    bookmaker = t.get("bookmaker", {})
+                                    if odds_type.lower() == "over/under":
+                                        total = bookmaker.get("total", [])
+                                        odds = find_main_1(total)
+                                        if len(odds) < 2:
+                                            return None
+                                        return "{} {} {} {} {} {} {}".format(
+                                            total[0]["name"],
+                                            odds[0]["name"],
+                                            odds[0]["value"],
+                                            odds[0]["us"],
+                                            odds[1]["name"],
+                                            odds[1]["value"],
+                                            odds[1]["us"],
+                                        )
+                                    elif odds_type.lower() == "handicap":
+                                        odds = find_main_1(bookmaker.get("handicap", {}))
+                                        if len(odds) < 2:
+                                            return None
+                                        return "{} {} {} {} {} {} {} {}".format(
+                                            odds[0]["name"],
+                                            odds[0]["handicap"],
+                                            odds[0]["value"],
+                                            odds[0]["us"],
+                                            odds[1]["name"],
+                                            odds[1]["handicap"],
+                                            odds[1]["value"],
+                                            odds[1]["us"],
+                                        )
+    return None
 
 
 if __name__ == "__main__":
